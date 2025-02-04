3a9a0dcd081688a3578d5af1f871bba30b3f90e6
BaGuai8 <vuxups70115@gmail.com>
2025-02-04 11:17:34 +0800

    ci: 🎡 部署個人介紹

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig(({ mode }) => {
   console.log("載入的 VITE 環境變數:", viteEnv);
 
   return {
-    base: "./",
+    base: "/self_introduction/",
     envDir: "./env", // 指定環境變數文件的目錄
     plugins: [
       // Vue 插件，用於支援 Vue 單文件組件
